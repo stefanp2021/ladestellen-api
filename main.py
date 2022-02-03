035fafe7aead3db7c5109473c9306322d2b20ba4
Stefan Pirker <stefan.pirker@muenzer.at>
2022-02-03 08:04:45 +0100

    main v2

--- main.py
+++ main.py
@@ -60,10 +60,10 @@ table_sub.to_csv(filepath)
 #### Now make the API url just for the operatorID
 get_operatorId = list(test[header_info[0]])
 
-forloop=False
+boolx=False
 counter = 0
 
-if forloop == True:
+if boolx == True:
     for i in get_operatorId:
         urlop = 'https://api.e-control.at/charge/1.0/countries/AT/operators/{operator_id}'.format(operator_id=str(i))
         counter = counter +1
@@ -73,9 +73,9 @@ if forloop == True:
         #print(urlop)
         #print(data_op)
         #print(i)
-        #print(test_op.shape)
-        #if (counter == 20):
-        #    print(test_op)
+        print(test_op.shape)
+        if (counter == 20):
+            print(test_op)
 
 
 
