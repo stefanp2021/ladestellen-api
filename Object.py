c3acbb8ed831e9bedb1c41b0153b6a7e2ee36650
davkohlf <david.kohlfuerst@muenzer.at>
2022-02-07 13:33:27 +0100

    new_design

---
+++ Object.py
@@ -0,0 +1,107 @@
+### Class -object
+
+
+
+from itertools import count
+from turtle import st
+
+
+
+
+class Street:
+    species = "Street Name"
+    def __init__(self, country, postCode, location, street):
+        self.street = street
+        self.country = country
+        self.postCode = postCode
+        self.location = location
+        self.PLZID = ""
+
+    def UpdateSQLStreet(self,connector):
+        mycursor = connector.cursor()
+        sql = "INSERT INTO tbl_street (PLZID, street) VALUES (%s, %s)"
+
+        val = (self.PLZID, self.street)
+        mycursor.execute(sql,val)
+        connector.commit()
+    def __del__(self):
+        pass
+        #Deconstrutor
+
+
+class Operator:
+    species = "Operator Values"
+    def __init__(self,operatorId,type,organization,commercialRegisterNumber,sex,
+    titlePrefix,firstName,lastName,titleSuffix,country,postCode,city,street,
+    website,deviatingOperatorIds,status):
+        self.operatorID = operatorId
+        self.type = type
+        self.organization = organization,
+        self.commercialRegisterNumber = commercialRegisterNumber
+        self.sex = sex
+        self.titlePrefix = titlePrefix
+        self.firstName=firstName
+        self.lastName=lastName
+        self.titleSuffix = titleSuffix
+        self.country = country
+        self.postCode = postCode
+        self.city = city
+        self.street=street
+        self.website = website
+        self.deviatingOperatorIds = deviatingOperatorIds
+        self.status = status
+        self.StreetID = ""
+
+    def UpdateSQLOperator(self,connector):
+        mycursor = connector.cursor()
+        sql = "INSERT INTO tbl_operator (operatorId,type,organization,commercialRegisterNumber,sex,titlePrefix,firstName,lastName,titleSuffix,street,website,deviatingOperatorIds,status) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s)"
+
+        val = (self.operatorID, self.type, self.organization,self.commercialRegisterNumber,self.sex,self.titlePrefix,self.firstName, 
+        self.lastName,self.titleSuffix,self.StreetID, self.StreetID,self.website,self.deviatingOperatorIds,self.status)
+        mycursor.execute(sql,val)
+        connector.commit()
+    def __del__(self):
+        pass
+        #Deconstrutor
+
+class Station:
+    species = "Station Values"
+    def __init__(self,stationId,stationStatus,label,description,postCode,city,street,latitude,longitude,
+    contactName,telephone,email,website,directions,greenEnergy,freeParking,priceUrl,public,openingHours,openingHoursdetails,
+    Operater_id):
+        self.stationId = stationId
+        self.stationStatus = stationStatus
+        self.label = label,
+        self.description = description
+        self.postCode = postCode
+        self.city = city
+        self.street=street
+        self.latitude=latitude
+        self.longitude = longitude
+        self.contactName = contactName
+        self.telephone = telephone
+        self.email = email
+        self.directions=directions
+        self.website = website
+        self.greenEnergy = greenEnergy
+        self.freeParking = freeParking
+        self.priceUrl = priceUrl
+        self.public = public
+        self.openingHours = openingHours
+        self.openingHoursdetails = openingHoursdetails
+        self.Operater_id = Operater_id
+        self.Operator_ID = ""
+        self.StreetID = ""
+
+    def UpdateSQLStation(self,connector):
+        mycursor = connector.cursor()
+        sql = "INSERT INTO tbl_station (stationId,stationStatus,label,description,street,latitude,longitude,contactName,telephone,email,website,directions,greenEnergy,freeParking,priceUrl,public,openingHours,openingHoursdetails,Operater_id) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s,%s, %s,%s,%s)"
+
+        val = (self.stationId, self.stationStatus, self.label,self.description,self.StreetID,self.latitude,self.longitude, 
+        self.contactName,self.telephone,self.email, self.website,self.directions,self.greenEnergy,self.freeParking,self.priceUrl,
+        self.public,self.openingHours,self.openingHoursdetails,self.Operater_id)
+        mycursor.execute(sql,val)
+        connector.commit()
+    def __del__(self):
+        pass
+        #Deconstrutor
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+import os
+#os.system('Object.py')
+
 #from curses import meta
 from importlib.metadata import metadata
 import json
-from multiprocessing.spawn import import_main_path
-from operator import and_
-import pathlib
-import re
-from xml.etree.ElementInclude import include
-import mysqlx
 from requests.auth import HTTPBasicAuth
 import requests
 import pandas as pd
@@ -16,35 +13,22 @@ from pathlib import Path
 from dataclasses import dataclass
 import mysql.connector
 
-
-#import pymysql
-#import sqlconnection
-
-import sqlalchemy
-from sqlalchemy import create_engine, select, MetaData, Table, and_
-
-#capacity = 10#input("Enter your capacity: ")
-#latitude = 47.104496# input("Enter your latitude: ")
-#longitude = 15.841415#input("Enter your logitude: ")
-
-#OperatorID = '000'
-
-#include auth.txt
+from Object import Operator, Station, Street
 
 
 countryID_AT = 'AT'
-countryID_GER = 'DE'
 bezeichner_op="Operater_id"
 
 keyfile=open('ladestellen-apikey.txt')
 API_KEY = keyfile.readline().rstrip()
 #url = 'https://api.e-control.at/charge/1.0/search?capacity={cap}&latitude={lat}&longitude={long}'.format(cap=capacity,lat=latitude,long=longitude)
 url_AT = 'https://api.e-control.at/charge/1.0/countries/{cAT}/operators'.format(cAT=countryID_AT)
-url_GER = 'https://api.e-control.at/charge/1.0/countries/{cGER}/operators'.format(cGER=countryID_GER)
+#url_GER = 'https://api.e-control.at/charge/1.0/countries/{cGER}/operators'.format(cGER=countryID_GER)
 
-print(url_AT)
+#print(url_AT)
 headers = {'Accept': 'application/json',}
 auth = HTTPBasicAuth('stefanpirker', 'xWQ24m2z3AqKP4HcarZz',)
+
 
 ### AT Url
 data_set_AT = requests.get(url_AT, headers=headers, auth=auth).json()
@@ -58,121 +42,58 @@ get_operatorId_AT = list(df_Operator_AT[header_info_AT[0]])
 print("#####---------------#################################--------------------------")
 location = header_info_AT[9:13]
 print(location)
-print(header_info_AT)
+#print(header_info_AT)
 country = location[0]
 PLZ = location[1]
 Ort = location[2]
 Straße = location[3]
-#bezeichner_adress = [PLZ,Ort,Straße]
-#df_table_adress = pd.DataFrame(columns=bezeichner_adress)
 df_table_adress = df_Operator_AT[[country,PLZ,Ort,Straße]]
-
-#print(df_Operator_AT)
-#print(get_operatorId_AT)
-#print(df_table_adress)
-df_Operator_AUT_CSV = df_Operator_AT.iloc[0:2,:]
-
-#print(df_Operator_AUT_CSV)
-
-
-##### Save csv test
-    #filepath = Path('output/Operator_AUT.csv')
-    #df_Operator_AUT_CSV.to_csv(filepath, header=True, index=True)
-
 
 print("#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-##-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-##---------------------------")
 
 ### To be able to turn it off
-boolx=False
-counter = 0
 waste_op_id = []
 
 #### Hier werden jetzt die Stationen ausgelesen, pro Land und Operator
-if boolx == True:
-    for i in get_operatorId_AT:
-        #counter = counter + 1
+
+for i in get_operatorId_AT:
+    #counter = counter + 1
+    #if counter == 20:
+        #print(i)
+        #print(counter)
+        ### Just work with 1 to see if it works in general
         #if counter == 20:
-            #print(i)
-            #print(counter)
-            ### Just work with 1 to see if it works in general
-            #if counter == 20:
-        urlstation = 'https://api.e-control.at/charge/1.0/countries/{cAT}/operators/{operator_id}/stations'.format(cAT = countryID_AT,operator_id=str(i))
+    urlstation = 'https://api.e-control.at/charge/1.0/countries/{cAT}/operators/{operator_id}/stations'.format(cAT = countryID_AT,operator_id=str(i))
 
-        #print(urlop)
-        data_op_LS = requests.get(urlstation, headers=headers, auth=auth).json()
-        df_LS_AUT = json_normalize(data_op_LS)
+    #print(urlop)
+    data_op_LS = requests.get(urlstation, headers=headers, auth=auth).json()
+    df_LS_AUT = json_normalize(data_op_LS)
 
-        #print(df_LS_AUT)
+    #if counter == 20:
+    #    filepath_Adresse = Path('output/Adresse_AUT_Komplett.csv')
+    #    df_LS_AUT.to_csv(filepath_Adresse, header=True, index=False)
 
-        df_shape_column_AUT = df_LS_AUT.shape[0]
-        list_op_Station = [i for x in range(int(df_shape_column_AUT))]
+    #print(df_LS_AUT)
 
-        #print(df_LS_AUT)
-        df_LS_AUT[bezeichner_op] = list_op_Station
-        #print(df_LS_AUT)
-        df_column_names = list(df_LS_AUT.columns)
-        #print(df_column_names)
+    df_shape_column_AUT = df_LS_AUT.shape[0]
+    list_op_Station = [i for x in range(int(df_shape_column_AUT))]
 
-    
-        ##### Now fill the whole df_table_adress with the adresses from the organizer first
-        ##### But some Operators are empty
-        if(df_LS_AUT.empty == True):
-            waste_op_id.append(i)
-        else:
-            df_table_station_Adress = df_LS_AUT[[PLZ,Ort,Straße]]
-
-            frames = [df_table_adress,df_table_station_Adress]
-            df_table_adress = pd.concat(frames)
-
-        #print(df_shape_column_AUT)
-        #if (counter == 25):
-        #    print(df_LS_AUT.shape)
-        #    print(df_LS_AUT)
-        #    ##### Save csv test
-            #  filepath_LS = Path('output/Station_AUT.csv')
-            #df_LS_AUT.to_csv(filepath_LS, header=True, index=True)
-    #print(counter)
+    #print(df_LS_AUT)
+    df_LS_AUT[bezeichner_op] = list_op_Station
+    df_LS_AUT["country"] = "AT"
+    #print(df_LS_AUT)
+    df_column_names = list(df_LS_AUT.columns)
+    #print(df_column_names)
 
 
-##### Now drop duplicates
-df_final_adresses = df_table_adress.drop_duplicates()
-df_final_adresses.reset_index(inplace = True, drop=True)
-print(df_final_adresses)
+    ##### Now fill the whole df_table_adress with the adresses from the organizer first
+    ##### But some Operators are empty
+    if(df_LS_AUT.empty == True):
+        waste_op_id.append(i)
+    else:
+        df_table_station_Adress = df_LS_AUT[[country,PLZ,Ort,Straße]]
 
-###### Now we have to insert all IDs from the table_PLZ to match the ID in Adress
-df_prepare_PLZ_City = df_final_adresses[[country,PLZ,Ort]]
-df_final_PLZ_City = df_prepare_PLZ_City.drop_duplicates()
-df_final_PLZ_City.reset_index(inplace=True,drop=True)
-print(df_final_PLZ_City)
+        frames = [df_table_adress,df_table_station_Adress]
+        df_table_adress = pd.concat(frames)
 
-
-#filepath_Adresse = Path('output/Adresse_AUT.csv')
-#df_final_adresses.to_csv(filepath_Adresse, header=True, index=False)
-#filepath_PLZ = Path('output/PLZ_AUT.csv')
-#df_final_PLZ_City.to_csv(filepath_PLZ, header=True, index=False)
-
-
-sql_up = False
-
-
-if sql_up == True:
-    mydb = mysql.connector.connect(
-        host="dev.muenzer.at",
-        user="ladestellen",
-        password ="ybV1NfB0sCrzWS22hzOiMZ7YwkmtIwMT",
-        database="ladestellen"
-    )
-
-    mycursor = mydb.cursor()
-    sql = "INSERT INTO tbl_plz (city, postCode, country) VALUES (%s, %s, %s)"
-
-    for i in range(df_final_PLZ_City.shape[0]):
-        data_row = df_final_PLZ_City.iloc[i,:]
-
-        val = (data_row[2], data_row[1], data_row[0])
-        mycursor.execute(sql,val)
-
-    mydb.commit()
-
-    #print(mycursor.rowcount, "record inserted.")
-
+###
